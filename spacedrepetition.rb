fbdebc90b59ab7a00de69aa59a923e98e9a99253
Prathik <prathik.m@tala.co>
2019-06-22 21:54:17 +0530

    Add init code for the brew script

---
+++ spacedrepetition.rb
@@ -0,0 +1,42 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Spacedrepetition < Formula
+  desc ""
+  homepage ""
+  url "prathik/spacedrepetition"
+  version ""
+  sha256 ""
+  depends_on "go" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+
+    ENV["GOPATH"] = buildpath
+    dir = buildpath/"src/gopkg.in/Netflix-Skunkworks/go-jira.v1"
+    dir.install buildpath.children
+
+    puts buildpath
+    
+    cd dir do
+      system "go", "build", "-o", bin/"jira", "-ldflags", "-w -s", "cmd/jira/main.go"
+      prefix.install_metafiles
+    end
+    
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test spacedrepetition`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "./spacedrepetition add 'test'"
+    system "./spacedrepetition all"
+  end
+end
